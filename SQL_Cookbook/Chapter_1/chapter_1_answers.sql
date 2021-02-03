aebb91c6c9af20387fbf3aedea6a961d510da159
PatrickCHennessey <66337447+PatrickCHennessey@users.noreply.github.com>
2021-02-03 12:23:28 -0500

    cleaned practice questions and answers

--- SQL_Cookbook/Chapter_1/chapter_1_answers.sql
+++ SQL_Cookbook/Chapter_1/chapter_1_answers.sql
@@ -1,11 +1,11 @@
 /* 1.1 - Retrieve All Rows and Columns from a Table*/
 
-	/* To get all rows and columns from "emp" table*/
+/* To get all rows and columns from "emp" table*/
 
 SELECT *
 FROM emp;
 
-	/* To get all rows and columns from "dept" table*/ 
+/* To get all rows and columns from "dept" table*/ 
 
 SELECT * 
 FROM dept;
@@ -21,10 +21,10 @@ WHERE deptno = 10;
 SELECT *
 	FROM emp
 		WHERE deptno = 10
-			OR comm is not null /* Error: Wrote "=" instead of "is"*/
-			OR sal <= "2000" AND deptno = "20"; 
+			OR comm is not null
+			OR sal <= 2000 AND deptno = 20; 
 
-	/*Use of parentheses in illustrating effect on query */
+/*Use of parentheses below in illustrating effect on above query */
 
 SELECT *
 	FROM emp
@@ -34,41 +34,53 @@ SELECT *
 		AND deptno = 20;
 
 /* 1.4 - Retrieving a Subset of Columns from a Table*/
+/* Retrieve only the columns your interested in */
 
 SELECT ename, deptno, sal
 	FROM emp;
 
 /* 1.5 - Providing Meaningful Names for Columns*/
+/* Change column headers in query */
 
 SELECT sal as salary, comm as commission
 	FROM emp;    
     
 /* 1.6 - Referencing an Aliased Column in the WHERE Clause*/
 
+/*Common Mistake 
 SELECT sal as salary, comm as commission
 	FROM emp
 		WHERE salary < 5000;
-        
+
+
+WHERE is evaluated before SELECT
+FROM is evaluated before WHERE
+So parentheses after FROM, illustrated below has query evaluate what is contained within the parentheses
+	before the WHERE is evaluated */
         
 SELECT *
 	FROM (
 			SELECT sal as salary, comm as commission
 				FROM emp
-					) x
+					) x /* "x" is an aliased inline view */
 	WHERE salary < 5000;
     
 /* 1.7 - Concatenating Column Values*/
 /* NOTE: Dependent on SQL used. MySQL is different than other types*/
-    
+/* Return values in multiple columns as one column */
+
+/* Data comes from the following    
 SELECT ename, job
 	FROM emp
 		WHERE deptno = 10;
+*/
     
 SELECT concat(ename, ' WORKS AS A ', job) as msg
 	FROM emp
 		WHERE deptno=10;
 
 /* 1.8 - Using Conditional Logic in a SELECT Statement*/
+/* IF-ELSE Conditional Formatting */
     
 SELECT ename, sal, 
 		CASE when sal <= 2000 then 'UNDERPAID'
@@ -79,7 +91,8 @@ SELECT ename, sal,
 
 /* 1.9 - Limiting the Number of Rows Returned*/
 /* NOTE: Dependent on SQL used. MySQL is different than other types*/
-    
+/* The order of the rows returned IS NOT specified */    
+
 SELECT *
 	FROM emp LIMIT 5;
 
@@ -91,12 +104,14 @@ SELECT ename, job
 		ORDER BY rand() LIMIT 5;
 
 /* 1.11 - Finding Null Values*/
-    
+/* Return all NULL values in a column */
+
 SELECT *
 	FROM emp
 		WHERE comm is null;
     
 /* 1.12 - Transforming Nulls into Real Values*/
+/* If a value is NULL, replaces NULL with a specified value*/
 
 SELECT coalesce(comm, 0)
 	FROM emp;
@@ -105,7 +120,16 @@ SELECT coalesce(mgr, 0)
 	FROM emp;
     
 /* 1.13 - Searching for Patterns*/
-    
+/* Example Query: 
+
+SELECT ename, job
+	FROM emp
+		WHERE deptno in (10,20);
+
+*/
+
+/* Return from above query those that have "I" in their name or a job title ending in "ER" */
+
 SELECT ename, job, deptno
 	FROM emp
 		WHERE deptno IN (10,20)
--- SQL_Cookbook/Chapter_1/chapter_1_problems.sql
+++ SQL_Cookbook/Chapter_1/chapter_1_problems.sql
@@ -9,94 +9,86 @@
 
 
 /* 1.2 - Retrieving a Subset of Rows from a Table*/
-	
-    /* Boss: Patrick can you find me all of the employees in the "emp" table who work in Accounting*/
 
 
 
 /* 1.3 - Finding Rows that Satisfy Multiple Conditions*/
-	
-    /*Boss: Patrick can you find me all of the employees in the "emp" table who work in Accounting, or receive a commission, 
-    or have a salary of $2,000 or less in the company's research department? */
-
-
-
-	/*Use of parentheses in illustrating effect on query */
 
 
 
 /* 1.4 - Retrieving a Subset of Columns from a Table*/
-
-	/* Boss: Too many columns. Can you reduce the columns that are returned so that I see only the ename, deptno, and sal columns? */
+/* Rertrieve only the columns your interested in */
 
 
 
 /* 1.5 - Providing Meaningful Names for Columns*/
+/* Change column headers in query */
 
-	/* Boss: Can you please change the columns sal and comm. There seems to be confusion as to what they represent.*/
 
-
+    
 /* 1.6 - Referencing an Aliased Column in the WHERE Clause*/
 
-	/* Boss: Uhhhh ask Dan */
+/*Common Mistake 
+SELECT sal as salary, comm as commission
+	FROM emp
+		WHERE salary < 5000;
 
 
+WHERE is evaluated before SELECT
+FROM is evaluated before WHERE
+So parentheses after FROM, illustrated below has query evaluate what is contained within the parentheses
+	before the WHERE is evaluated */
         
-        
 
+    
 /* 1.7 - Concatenating Column Values*/
 /* NOTE: Dependent on SQL used. MySQL is different than other types*/
-	/* Boss: Concatenate the values from multiple columns to read like a memo.*/
+/* Return values in multiple columns as one column */
+
+/* Data comes from the following    
+SELECT ename, job
+	FROM emp
+		WHERE deptno = 10;
+*/
     
 
 
 /* 1.8 - Using Conditional Logic in a SELECT Statement*/
-
-/* Boss: Create an IF-Else column that provides a value based on the employee's salary. 
-    The values should be:
-    sal < 2000 returns "underpaid"
-    sal > 2000 and sal < 4000 returns "ok" 
-    sal >= 4000 returns "overpaid" */    
+/* IF-ELSE Conditional Formatting */
     
 
 
 /* 1.9 - Limiting the Number of Rows Returned*/
 /* NOTE: Dependent on SQL used. MySQL is different than other types*/
-	/* Boss: Limit the number of rows returned when finding all employees in the emp table to 5*/
-    
+/* The order of the rows returned IS NOT specified */    
+
 
 
 /* 1.10 - Returning n Random Records from a Table*/
 /* NOTE: Dependent on SQL used. MySQL is different than other types*/
 
-	/* Boss: Return 5 different random records based on modifying the following query:
-    
-			SELECT ename, job 
-				FROM emp LIMIT 5; 
-    
-    */
-
 
 
 /* 1.11 - Finding Null Values*/
+/* Return all NULL values in a column */
 
-	/* Boss: I need to find all the null values in the comm column */
+
     
-
 /* 1.12 - Transforming Nulls into Real Values*/
+/* If a value is NULL, replaces NULL with a specified value*/
 
-	/* Boss: Replace null values with 0*/
 
-
-    
     
 /* 1.13 - Searching for Patterns*/
+/* Example Query: 
 
-	/* Boss: Looking for employees within the Accounting or Research departments that have an I in their name 
-				or have a job that contians an 'ER'
+SELECT ename, job
+	FROM emp
+		WHERE deptno in (10,20);
 
-			SELECT ename, job
-				FROM emp
-					WHERE deptno in (10,20)
-	*/ 
-    
+*/
+
+/* Return from above query those that have "I" in their name or a job title ending in "ER" */
+
+
+
