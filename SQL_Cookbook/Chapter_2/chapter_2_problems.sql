a5887435a90d22c052e359e29cfa0872249c4d30
PatrickCHennessey <66337447+PatrickCHennessey@users.noreply.github.com>
2021-02-03 12:24:58 -0500

    cleaned practice questions and answers

--- SQL_Cookbook/Chapter_2/chapter_2_problems.sql
+++ SQL_Cookbook/Chapter_2/chapter_2_problems.sql
@@ -1,35 +1,68 @@
 /* 2.1 Returning Query Results in a Specific Order*/
-	/* Boss: Display the names, jobs, salaries of employees in the Accounting department displayed based on salary (lowest-highest)*/
+/* Display the names, jobs, salaries of employees in the department 10 displayed based on salary (lowest-highest) */
 
+/* ORDER BY sorted by Ascending value by default */
+
+
+
+/* Sort by Descending*/
+
+
+
+/* Sort by header number from left to right, starts at 1 instead of 0, this is used as alternate way to indicate which column header to ORDER BY*/
+	
 
 
 /* 2.2 Sorting by Multiple Fields*/
-	/* Boss: Sort the rows in the EMP table by ascending deptno value first followed by descending salary value*/
-
+/* Sort the rows from EMP first by DEPTNO ascebding then by salary descending */
+	
 
 
 /* 2.3 Sorting by Substrings*/
-	/* Boss: Return from the EMP table ename and jobs sorted by the last 2 characters in job*/
-	/* NOTE: Dependent on SQL used. MySQL is different than other types*/
+/* NOTE: Dependent on SQL used. MySQL is different than other types*/
+/* Sort results of a query by specific parts of a string. Return employee names and jobs from the EMP table and sort by the last 2 characters in the job field. */
 
 
 
-/* SKIP!!!! */ 
-/* 2.4 Sorting Mixed Alphanumeric Data*/  
-	/* Boss: Sort the data by either the numeric or character portions*/
-	/* NOTE: Dependent on SQL used. Supported only by PostgreSQL, Oracle, and DB2. Unsupported by MySQL and SQL server*/
+/* 2.4 Sorting Mixed Alphanumeric Data*/
+/* NOTE: Dependent on SQL used. 
+Supported only by PostgreSQL, Oracle, and DB2. 
+Unsupported by MySQL and SQL server*/
 
 
 
 /* 2.5 Dealing with Nulls When Sorting*/
-	/* Boss: Sort EMP results by COMM. We need to specify if nulls are sorted last.*/
-	/* NOTE: Dependent on SQL used. MySQL is different than other types*/
+/* NOTE: Dependent on SQL used. MySQL is different than other types*/
+
+/* EMP table sort by column containing NULL values in ascending order */ 
+
+
+/* EMP table sort by column containing NULL values in descending order */ 
+
+
+/*2.5 (continued) Various Methods of Sorting*/
+
+/* Non-Null Comm Sorted Ascending, All Nulls Last*/
+	
+
+                        
+/* Non-Null Comm Sorted Descending, All Nulls Last*/
 
 
 
-/* 2.6 Sorting by Data Dependent Key*/
-	/* Boss: Sort on multiple conditions:
-		1. If Job= 'Salesman' sort by comm
-        2. else sort by sal*/
+/* Non-Null Comm sorted by Ascending, All Nulls First*/
 
+
+
+/* Non-Null Comm sorted by Descending, All Nulls First*/
+
+
+
+
+/* 2.6 Sorting on a Data Dependent Key*/
+/* If job is SALESMAN sort by comm, otherwise sort by sal*/
+
+
+
+/* Alternative Query */
 
