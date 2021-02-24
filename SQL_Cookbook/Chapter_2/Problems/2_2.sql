13b1029b500bbc7eea851111291866401799fc98
PatrickCHennessey <66337447+PatrickCHennessey@users.noreply.github.com>
2021-02-24 14:44:17 -0500

    Add Chapter 2 Problems

---
+++ SQL_Cookbook/Chapter_2/Problems/2_2.sql
@@ -0,0 +1,4 @@
+-- Sorting Query Results
+
+-- 2.2 Sorting by Multiple Fields
+-- Sort the rows from EMP first by DEPTNO ascending then by salary descending
---
+++ SQL_Cookbook/Chapter_2/Problems/2_6.sql
@@ -0,0 +1,11 @@
+-- Sorting Query Results
+
+-- 2.6 Sorting on a Data Dependent Key
+-- Sort using conditional formatting,
+
+-- If JOB is "SALESMAN" sort by COMM, otherwise sort by SAL
+
+
+
+-- Alternative Query
+
