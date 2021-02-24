82de0a52f576d67385e3aefe57e0aa8ce598aedb
PatrickCHennessey <66337447+PatrickCHennessey@users.noreply.github.com>
2021-02-24 15:28:07 -0500

    Clarified 1_6 Problem and Answers

--- SQL_Cookbook/Chapter_1/Problems/1_6.sql
+++ SQL_Cookbook/Chapter_1/Problems/1_6.sql
@@ -4,19 +4,10 @@
 
 /* Common Mistake
  
-SELECT sal as salary, comm as commission
-	FROM emp
-		WHERE salary < 5000; 
+select sal as salary, comm as commission
+	from emp
+		where salary < 5000; 
 
 */
 
-/* 
 
-Note:
-
-WHERE is evaluated before SELECT, FROM is evaluated before WHERE, 
-So parentheses after FROM, illustrated below has the query evaluate what is contained within the parentheses
-*BEFORE* the WHERE is evaluated
-
-*/
-        
